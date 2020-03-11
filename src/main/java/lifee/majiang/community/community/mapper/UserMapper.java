d0bae0daf46a9f965756ce1098d4c7ba27fa7e83
ffff55jjjjj <670124249@qq.com>
2020-03-11 22:46:09 +0800

    add cookie to save user token

--- src/main/java/lifee/majiang/community/community/mapper/UserMapper.java
+++ src/main/java/lifee/majiang/community/community/mapper/UserMapper.java
@@ -3,9 +3,16 @@ package lifee.majiang.community.community.mapper;
 import lifee.majiang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("INSERT INTO USER(name,account_id,token,gtm_create,gtm_modified) VALUES(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * FROM USER WHERE TOKEN = #{token}")
+    User findByToken(@Param("token") String token);
 }
+
+
