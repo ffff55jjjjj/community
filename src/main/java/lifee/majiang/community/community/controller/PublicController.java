d4f6160ceb081645db3c21ba01a0c78372a8e444
ffff55jjjjj <670124249@qq.com>
2020-03-15 19:04:43 +0800

    publish function

--- src/main/java/lifee/majiang/community/community/controller/PublicController.java
+++ src/main/java/lifee/majiang/community/community/controller/PublicController.java
@@ -1,12 +1,69 @@
 package lifee.majiang.community.community.controller;
 
+import lifee.majiang.community.community.mapper.QuestionMapper;
+import lifee.majiang.community.community.mapper.UserMapper;
+import lifee.majiang.community.community.model.Question;
+import lifee.majiang.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublicController {
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
    public String publish(){
        return "publish";
+   }
+
+   @PostMapping("/publish")
+   public String doPublish(
+           @RequestParam("title") String title,
+           @RequestParam("description") String description,
+           @RequestParam("tag") String tag,
+           HttpServletRequest request,
+           Model model){
+
+
+       Cookie[] cookies = request.getCookies();
+       User user = null;
+       if(cookies!= null){
+           for (Cookie cookie : cookies) {
+               if(cookie.getName().equals("token")){
+                   String token = cookie.getValue();
+                   user = userMapper.findByToken(token);
+                   if(user != null){
+                       request.getSession().setAttribute("user",user);
+                   }
+                   break;
+               }
+           }
+       }
+       if(user == null){
+           model.addAttribute("error","请先登录，再发布！");
+           return "publish";
+       }
+       Question question = new Question();
+       question.setTitle(title);
+       question.setDescription(description);
+       question.setTag(tag);
+       question.setCreator(user.getId());
+       question.setGmtCreate(System.currentTimeMillis());
+       question.setGmtModified(question.getGmtCreate());
+       questionMapper.create(question);
+
+       return "redirect:/";
    }
 }
