32388273a4107c5cc0508d0e4b2f1ee872065b1e
ffff55jjjjj <670124249@qq.com>
2020-03-21 23:04:07 +0800

    add auto login function

---
+++ src/main/java/lifee/majiang/community/community/controller/AutoLoginController.java
@@ -0,0 +1,23 @@
+package lifee.majiang.community.community.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class AutoLoginController {
+
+    @Value("${auto.login.default.token}")
+    private String defaultToken;
+
+    @GetMapping("/autoLogin")
+    public String autoLogin(HttpServletResponse response, Model model){
+        response.addCookie(new Cookie("token",defaultToken));
+        return "redirect:/";
+    }
+}
